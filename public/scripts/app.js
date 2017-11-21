248bf0498f2448e8d0f4f8c23f0438c81c8d870e
Sloan Stewart <sloanstewart@d25sp18agq17im.amer.homedepot.com>
2017-11-21 14:22:27 -0500

    simple counter with manual data binding

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,15 +6,73 @@ var template = React.createElement(
     React.createElement(
         'h1',
         null,
-        'React App'
+        'Indescision App'
     ),
     React.createElement(
         'p',
         null,
-        'Some great text here.'
+        'sick app, bro.'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'item two'
+        )
     )
 );
 
+var count = 0;
+var addOne = function addOne() {
+    count++;
+    renderCounter();
+};
+var subtractOne = function subtractOne() {
+    count--;
+    renderCounter();
+};
+var reset = function reset() {
+    count = 0;
+    renderCounter();
+};
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderCounter = function renderCounter() {
+    var templateTwo = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            '+1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: subtractOne },
+            '-1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'Reset'
+        )
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounter();
--- src/app.js
+++ src/app.js
@@ -1,10 +1,43 @@
-var template = (
+
+
+const template = (
     <div>
-        <h1>React App</h1>
-        <p>Some great text here.</p>
+        <h1>Indescision App</h1>
+        <p>sick app, bro.</p>
+        <ol>
+            <li>item one</li>
+            <li>item two</li>
+        </ol>
     </div>
 );
 
-var appRoot = document.getElementById('app');
+let count = 0;
+const addOne = () => {
+    count++;
+    renderCounter();
+};
+const subtractOne = () => {
+    count --;
+    renderCounter();
+};
+const reset = () => {
+    count = 0;
+    renderCounter();
+};
 
-ReactDOM.render(template, appRoot);
+const appRoot = document.getElementById('app');
+
+const renderCounter = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={subtractOne}>-1</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounter();
