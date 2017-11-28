ef53aecbaf95a2f863d186484233be31ad00ac12
Sloan Stewart <sloanstewart@d25sp18agq17im.amer.homedepot.com>
2017-11-28 09:39:04 -0500

    class extension playground

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,12 @@
 'use strict';
 
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -31,8 +37,70 @@ var Person = function () {
     return Person;
 }();
 
-var me = new Person('Dudester', 69);
+var Student = function (_Person) {
+    _inherits(Student, _Person);
+
+    function Student(name, age, major) {
+        _classCallCheck(this, Student);
+
+        var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
+
+        _this.major = major;
+        return _this;
+    }
+
+    _createClass(Student, [{
+        key: 'hasMajor',
+        value: function hasMajor() {
+            return !!this.major;
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
+
+            if (this.hasMajor()) {
+                description += ' Majoring in ' + this.major;
+            }
+
+            return description;
+        }
+    }]);
+
+    return Student;
+}(Person);
+
+var Traveler = function (_Person2) {
+    _inherits(Traveler, _Person2);
+
+    function Traveler(name, age, home) {
+        _classCallCheck(this, Traveler);
+
+        var _this2 = _possibleConstructorReturn(this, (Traveler.__proto__ || Object.getPrototypeOf(Traveler)).call(this, name, age));
+
+        _this2.home = home;
+        return _this2;
+    }
+
+    _createClass(Traveler, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            var greeting = _get(Traveler.prototype.__proto__ || Object.getPrototypeOf(Traveler.prototype), 'getGreeting', this).call(this);
+
+            if (this.home) {
+                return greeting += ' I\'m visiting from ' + this.home;
+            }
+        }
+    }]);
+
+    return Traveler;
+}(Person);
+
+var me = new Student('Dudester', 69, 'Twerkology');
 console.log(me.getDescription());
 
-var other = new Person();
-console.log(other);
+var other = new Student();
+console.log(other.getDescription());
+
+var travelmane = new Traveler('travel mane', 420, 'Dublin');
+console.log(travelmane.getGreeting());
--- src/playground/es6-classes.js
+++ src/playground/es6-classes.js
@@ -9,12 +9,49 @@ class Person {
         return `${this.name} says: ayyy lmao`;
     }
     getDescription() {
-        return `${this.name} is ${this.age} years old.`
+        return `${this.name} is ${this.age} years old.`;
     }
 }
 
-const me = new Person('Dudester', 69);
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor(){
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+
+        if (this.hasMajor()) {
+            description += ` Majoring in ${this.major}`;
+        }
+
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, home) {
+        super(name, age);
+        this.home = home;
+    }
+    getGreeting(){
+        let greeting = super.getGreeting();
+
+        if(this.home) {
+            return greeting +=` I'm visiting from ${this.home}`;
+        }
+    }
+}
+
+
+const me = new Student('Dudester', 69, 'Twerkology');
 console.log(me.getDescription());
 
-const other = new Person();
-console.log(other);
+const other = new Student();
+console.log(other.getDescription());
+
+const travelmane = new Traveler('travel mane', 420, 'Dublin');
+console.log(travelmane.getGreeting());
