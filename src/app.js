a9101321702fa62e73c0250f9467bde2e8a930f8
Sloan Stewart <sloanstewart@d25sp18agq17im.amer.homedepot.com>
2017-11-28 13:18:01 -0500

    s4 l30: method binding

--- src/app.js
+++ src/app.js
@@ -40,8 +40,12 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+    constructor(props) {
+        super(props);
+        this.removeAll = this.removeAll.bind(this);
+    }
     removeAll() {
-        console.log('removeAll');
+        console.log(this.props.options);
     }
     render() {
         return (
