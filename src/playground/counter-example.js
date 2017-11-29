bc5fea869eed06658f89a54f09f067b70f4bd092
Sloan Stewart <sloanstewart@d25sp18agq17im.amer.homedepot.com>
2017-11-29 09:02:10 -0500

    increment and reset counter state

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,26 +1,41 @@
 class Counter extends React.Component {
-    counstructor(props){
+    constructor(props){
         super(props);
         this.addOne = this.addOne.bind(this);
         this.minusOne = this.minusOne.bind(this);
         this.reset = this.reset.bind(this);
+        this.state = {
+            count: 0
+        }
     }
     addOne() {
-        count ++;
+        this.setState((prevState) => {
+            return {
+                count: prevState.count + 1
+            };
+        });
     }
 
     minusOne() {
-        count --;
+        this.setState((prevState) => {
+            return {
+                count: prevState.count - 1
+            };
+        });
     }
 
     reset() {
-        count = 0;
+        this.setState((prevState) => {
+            return {
+                count: prevState.count = 0
+            };
+        });
     }
 
     render() {
         return (
             <div>
-                <h1>Count: </h1>
+                <h1>Count: {this.state.count}</h1>
                 <button onClick={this.addOne}>+1</button>
                 <button onClick={this.minusOne}>-1</button>
                 <button onClick={this.reset}>reset</button>
