4a7e6dc30ca586d6510f5985bef3dd17bad98f93
Sloan Stewart <sloanstewart@d25sp18agq17im.amer.homedepot.com>
2017-11-29 08:52:44 -0500

    s4 l32: counter component state

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,29 +1,62 @@
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCounter();
-};
-const subtractOne = () => {
-    count --;
-    renderCounter();
-};
-const reset = () => {
-    count = 0;
-    renderCounter();
-};
+class Counter extends React.Component {
+    counstructor(props){
+        super(props);
+        this.addOne = this.addOne.bind(this);
+        this.minusOne = this.minusOne.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+    addOne() {
+        count ++;
+    }
+
+    minusOne() {
+        count --;
+    }
+
+    reset() {
+        count = 0;
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Count: </h1>
+                <button onClick={this.addOne}>+1</button>
+                <button onClick={this.minusOne}>-1</button>
+                <button onClick={this.reset}>reset</button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
+
+// let count = 0;
+// const addOne = () => {
+//     count++;
+//     renderCounter();
+// };
+// const subtractOne = () => {
+//     count --;
+//     renderCounter();
+// };
+// const reset = () => {
+//     count = 0;
+//     renderCounter();
+// };
 
 
-const renderCounter = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={subtractOne}>-1</button>
-            <button onClick={reset}>Reset</button>
-        </div>
-    );
+// const renderCounter = () => {
+//     const templateTwo = (
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={addOne}>+1</button>
+//             <button onClick={subtractOne}>-1</button>
+//             <button onClick={reset}>Reset</button>
+//         </div>
+//     );
 
-    ReactDOM.render(templateTwo, appRoot);
-};
+//     ReactDOM.render(templateTwo, appRoot);
+// };
 
-renderCounter();
+// renderCounter();
