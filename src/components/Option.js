da39061c86fc932e30171c952572fc1f68cec890
Sloan Stewart <sloanstewart@d25sp18agq17im.amer.homedepot.com>
2017-11-30 09:47:59 -0500

    refactored stateless functional components

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const Option = (props) => {
-    return (
-        <li>
-        {props.optionText}
-        <button 
-            onClick={(e) => {
-                props.deleteOneOption(props.optionText)
-            }}
-        >
-            X
-        </button>
-        </li>
-    );
-};
+const Option = (props) => (
+    <li>
+    {props.optionText}
+    <button 
+        onClick={(e) => {
+            props.deleteOneOption(props.optionText)
+        }}
+    >
+        X
+    </button>
+    </li>
+);
 
 export default Option;
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import Option from './Option';
 
-const Options = (props) => {
-    return (
-        <div>
-            {props.options.length === 0 && <p>No options found. Add an option to get started.</p>}
-            <ol>
-                {
-                    props.options.map((option) => (
-                        <Option 
-                            key={option}
-                            optionText={option}
-                            deleteOneOption={props.deleteOneOption}
-                        />
-                    ))
-                }
-            </ol>
-            <button onClick={props.deleteOptions}>Remove All</button>
-        </div>
-    );
-};
+const Options = (props) => (
+    <div>
+        {props.options.length === 0 && <p>No options found. Add an option to get started.</p>}
+        <ol>
+            {
+                props.options.map((option) => (
+                    <Option 
+                        key={option}
+                        optionText={option}
+                        deleteOneOption={props.deleteOneOption}
+                    />
+                ))
+            }
+        </ol>
+        <button onClick={props.deleteOptions}>Remove All</button>
+    </div>
+);
 
 export default Options;
